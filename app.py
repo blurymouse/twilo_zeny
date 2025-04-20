19f53fde094694456c36fa588e15ba1431aaeda1
blurymouse <119121514+blurymouse@users.noreply.github.com>
2025-04-21 02:17:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+@app.route("/voice")
+def voice():
+    twiml = """<?xml version="1.0" encoding="UTF-8"?>
+    <Response>
+        <Say voice="Polly.Joanna">
+            Hello. This is an urgent alert from Zeny.
+            New patient data has been sent to you via WhatsApp and email.
+            Please check your messages as soon as possible.
+        </Say>
+    </Response>"""
+    return Response(twiml, mimetype='text/xml')
